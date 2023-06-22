7fc463b229d9eb22104ba1d0b320764efd6b6edf
Ryota Wakizaka <rwakizaka@techfirm.co.jp>
2023-06-22 10:57:51 +0900

    半モーダル機能を追加する

--- lib/f015_material_test9_page.dart
+++ lib/f015_material_test9_page.dart
@@ -7,60 +7,42 @@ class MaterialTest9Page extends HookConsumerWidget {
 
   const MaterialTest9Page({super.key, required this.title});
 
-  Widget createListView(List<TextEditingController> controllers) {
-    final context = useContext();
-    return GestureDetector(
-        onTap: () {
-          final FocusScopeNode focusScope = FocusScope.of(context);
-          if (!focusScope.hasPrimaryFocus && focusScope.hasFocus) {
-            FocusManager.instance.primaryFocus!.unfocus();
-          }
-        },
-        child: ListView(
-            scrollDirection: Axis.vertical,
-            padding: const EdgeInsets.only(
-              left: 16,
-              top: 16,
-              right: 16,
-              bottom: 16,
-            ),
-            children: [
-              for (int i = 0; i < controllers.length / 2; i = i + 2) ... {
-                TextField(
-                  controller: controllers[i],
-                  style: const TextStyle(fontSize: 15),
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(8),
-                    border: OutlineInputBorder(),
-                    hintText: '宛先',
-                  ),
-                  onChanged: (text) {
-                    debugPrint("Textの変更検知={$text}");
-                  },
-                ),
+  List<Widget> createListViewRow(List<TextEditingController> controllers) {
+    return [
+      for (int i = 0; i < controllers.length / 2; i = i + 2) ... {
+        TextField(
+          controller: controllers[i],
+          style: const TextStyle(fontSize: 15),
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(8),
+            border: OutlineInputBorder(),
+            hintText: '宛先',
+          ),
+          onChanged: (text) {
+            debugPrint("Textの変更検知={$text}");
+          },
+        ),
 
-                Container(height: 8),
+        Container(height: 8),
 
-                TextField(
-                  controller: controllers[i + 1],
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
-                  style: const TextStyle(fontSize: 15),
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(8),
-                    border: OutlineInputBorder(),
-                    hintText: '本文\n\n\n\n\n',
-                  ),
-                  onChanged: (text) {
-                    debugPrint("Textの変更検知={$text}");
-                  },
-                ),
+        TextField(
+          controller: controllers[i + 1],
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+          style: const TextStyle(fontSize: 15),
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(8),
+            border: OutlineInputBorder(),
+            hintText: '本文\n\n\n\n\n',
+          ),
+          onChanged: (text) {
+            debugPrint("Textの変更検知={$text}");
+          },
+        ),
 
-                Container(height: 8),
-              }
-            ]
-        )
-    );
+        Container(height: 8),
+      }
+    ];
   }
 
   @override
@@ -91,44 +73,62 @@ class MaterialTest9Page extends HookConsumerWidget {
       }
     ]);
 
+    var listViewRow = createListViewRow(mainTextFieldControllers.value);
+
     return Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-      body: SafeArea(child:
-          // TODO: キーボードでずれてしまう。https://hiyoko-programming.com/1694/
-        Column(children: [
-          Expanded(child: createListView(mainTextFieldControllers.value)),
-
-          Container(
-            color: theme.colorScheme.background,
-            padding: const EdgeInsets.fromLTRB(16, 0, 16, 3),
-            child: Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    showModalBottomSheet(
-                      isScrollControlled: true,
-                      context: context,
-                      builder: (BuildContext context) {
-                        return Container(
-                          height: 700,
-                          color: theme.colorScheme.background,
-                        );
-                      },
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(150, 32),
-                    textStyle: const TextStyle(fontSize: 13),
-                    padding: const EdgeInsets.all(0),
+        body: SafeArea(
+          child: GestureDetector(
+              onTap: () {
+                final FocusScopeNode focusScope = FocusScope.of(context);
+                if (!focusScope.hasPrimaryFocus && focusScope.hasFocus) {
+                  FocusManager.instance.primaryFocus!.unfocus();
+                }
+              },
+              child: ListView(
+                  scrollDirection: Axis.vertical,
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    top: 16,
+                    right: 16,
+                    bottom: 16,
                   ),
-                  child: const Text('モーダル表示'),
-                ),
-              ],
-            ),
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 3),
+                      child: Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              showModalBottomSheet(
+                                isScrollControlled: true,
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return Container(
+                                    height: 700,
+                                  );
+                                },
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(150, 32),
+                              textStyle: const TextStyle(fontSize: 13),
+                              padding: const EdgeInsets.all(0),
+                            ),
+                            child: const Text('半モーダル表示'),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    for (int i=0; i < listViewRow.length; i++) ... {
+                      listViewRow[i],
+                    }
+                  ]
+              )
           )
-        ])
       )
     );
   }
