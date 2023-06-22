a4d161c6fc275dc986301335dc7e3758176a14f7
Ryota Wakizaka <rwakizaka@techfirm.co.jp>
2023-06-22 18:42:37 +0900

    半モーダル機能を追加する

--- lib/f015_material_test9_page.dart
+++ lib/f015_material_test9_page.dart
@@ -7,7 +7,46 @@ class MaterialTest9Page extends HookConsumerWidget {
 
   const MaterialTest9Page({super.key, required this.title});
 
-  List<Widget> createListViewRow(List<TextEditingController> controllers) {
+  Widget creteBaseWidget(BuildContext context, Widget topWidget,
+      List<TextEditingController> controllers,
+      List<TextEditingController>? originals) {
+    var listViewRows = createListViewRows(controllers);
+
+    // TODO: 処理タイミングを探す
+    // if (originals != null) {
+    //   for (int i = 0; i < controllers.length; i++) {
+    //     controllers[i].text = originals[i].text;
+    //   }
+    // }
+
+    return SafeArea(
+        child: GestureDetector(
+            onTap: () {
+              final FocusScopeNode focusScope = FocusScope.of(context);
+              if (!focusScope.hasPrimaryFocus && focusScope.hasFocus) {
+                FocusManager.instance.primaryFocus!.unfocus();
+              }
+            },
+            child: ListView(
+                scrollDirection: Axis.vertical,
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  top: 0,
+                  right: 16,
+                  bottom: 16,
+                ),
+                children: [
+                  topWidget,
+                  for (int i=0; i < listViewRows.length; i++) ... {
+                    listViewRows[i],
+                  }
+                ]
+            )
+        )
+    );
+  }
+
+  List<Widget> createListViewRows(List<TextEditingController> controllers) {
     return [
       for (int i = 0; i < controllers.length / 2; i = i + 2) ... {
         TextField(
@@ -48,8 +87,9 @@ class MaterialTest9Page extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
-    final state = ref.watch(materialTest9PageNotifierProvider);
-    final notifier = ref.watch(materialTest9PageNotifierProvider.notifier);
+
+    // 画面の高さ
+    double deviceHeight = MediaQuery.of(context).size.height;
 
     useEffect(() {
       debugPrint('MaterialTest9Pageの初期化処理');
@@ -73,86 +113,88 @@ class MaterialTest9Page extends HookConsumerWidget {
       }
     ]);
 
-    var listViewRow = createListViewRow(mainTextFieldControllers.value);
+    var closeContainer = Container(
+        padding: const EdgeInsets.fromLTRB(
+            18, 14, 0, 3),
+        child: Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                for (int i = 0; i < mainTextFieldControllers
+                    .value.length; i++) {
+                  mainTextFieldControllers.value[i].text
+                    = halfModalTextFieldControllers.value[i].text;
+                }
+
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(150, 32),
+                textStyle: const TextStyle(
+                    fontSize: 13),
+                padding: const EdgeInsets.all(0),
+              ),
+              child: const Text('閉じる'),
+            ),
+          ],
+        )
+    );
+
+    showHalfModal() {
+      showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext context) {
+          var body = creteBaseWidget(context,
+              Container(height: 8), halfModalTextFieldControllers.value,
+              mainTextFieldControllers.value);
+          return Stack(children: [
+            Container(
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.background,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.only(top: 68),
+                height: deviceHeight * 0.8,
+                child: Scaffold(
+                    body: body
+                )
+            ),
+            closeContainer
+          ]);
+        },
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+                top: Radius.circular(16)
+            )
+        ),
+      );
+    }
 
     return Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-        body: SafeArea(
-          child: GestureDetector(
-              onTap: () {
-                final FocusScopeNode focusScope = FocusScope.of(context);
-                if (!focusScope.hasPrimaryFocus && focusScope.hasFocus) {
-                  FocusManager.instance.primaryFocus!.unfocus();
-                }
-              },
-              child: ListView(
-                  scrollDirection: Axis.vertical,
-                  padding: const EdgeInsets.only(
-                    left: 16,
-                    top: 16,
-                    right: 16,
-                    bottom: 16,
-                  ),
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 3),
-                      child: Row(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              showModalBottomSheet(
-                                isScrollControlled: true,
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return Container(
-                                    height: 700,
-                                  );
-                                },
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              fixedSize: const Size(150, 32),
-                              textStyle: const TextStyle(fontSize: 13),
-                              padding: const EdgeInsets.all(0),
-                            ),
-                            child: const Text('半モーダル表示'),
-                          ),
-                        ],
-                      ),
+        body: creteBaseWidget(context,
+            Container(
+              padding: const EdgeInsets.fromLTRB(0, 14, 0, 3),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      showHalfModal();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(150, 32),
+                      textStyle: const TextStyle(fontSize: 13),
+                      padding: const EdgeInsets.all(0),
                     ),
-
-                    for (int i=0; i < listViewRow.length; i++) ... {
-                      listViewRow[i],
-                    }
-                  ]
-              )
-          )
-      )
+                    child: const Text('半モーダル表示'),
+                  ),
+                ],
+              ),
+            ), mainTextFieldControllers.value, null
+        )
     );
   }
 }
-
-// view_model
-class MaterialTest9PageState {
-  static MaterialTest9PageState copy(MaterialTest9PageState state) {
-    var nState = MaterialTest9PageState();
-    return nState;
-  }
-}
-class MaterialTest9PageNotifier extends StateNotifier<MaterialTest9PageState> {
-  final Ref ref;
-
-  MaterialTest9PageNotifier(this.ref, MaterialTest9PageState state)
-      : super(state);
-
-  updateState() {
-    state = MaterialTest9PageState.copy(state);
-  }
-}
-final materialTest9PageNotifierProvider =
-StateNotifierProvider.autoDispose<MaterialTest9PageNotifier,
-    MaterialTest9PageState>((ref) {
-  return MaterialTest9PageNotifier(ref, MaterialTest9PageState());
-});
